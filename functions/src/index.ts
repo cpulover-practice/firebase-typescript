5cd15f3d34312b78c97e30963cffcfc84cf5ed02
cpulover <justacpulover@gmail.com>
2020-08-20 19:23:39 +0200

    API get document

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,24 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase by TypeScript!");
+});
+
+export const hochiminhWeather = functions.https.onRequest((request, response) => {
+    //promise contains document snapshot
+    const promise = admin.firestore().doc("cities-weather/hochiminh").get();
+    promise.then(snapshot => {
+        const data = snapshot.data();
+        console.log(`Weather in Ho Chi Minh city is ` + snapshot.get('condition'));
+        response.send(data); //send() will also terminate http trigger
+    }).catch(error => { // must handle rejected case for the promise
+        console.log(error);
+        response.status(500).send(error);
+    })
+});
